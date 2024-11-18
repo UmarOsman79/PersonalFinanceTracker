ad485f293ae5b630f83670772a8092d38cd97da3
Umar Osman <umarosman77@gmail.com>
2024-11-18 21:35:06 +0200

    Setting up

--- src/main/java/com/gmail/umarosman77/pft/PersonalFinanceTrackerApplication.java
+++ src/main/java/com/gmail/umarosman77/pft/PersonalFinanceTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.gmail.umarosman77.PersonalFinanceTracker;
+package com.gmail.umarosman77.pft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class PersonalFinanceTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
 	}
-
 }
