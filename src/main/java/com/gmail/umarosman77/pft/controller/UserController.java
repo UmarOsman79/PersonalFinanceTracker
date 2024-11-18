522b7d51e8d32bcc8d8cea49590499a56f465d21
Umar Osman <umarosman77@gmail.com>
2024-11-18 23:55:59 +0200

    Added base of user service

--- src/main/java/com/gmail/umarosman77/pft/controller/UserController.java
+++ src/main/java/com/gmail/umarosman77/pft/controller/UserController.java
@@ -6,15 +6,19 @@ import com.gmail.umarosman77.pft.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//Parameter for @RestController is to name the bean
+//Because we're using @RestController and not @Controller, don't need to add @ResponseBody to each method
+@RestController("userController")
+@RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@PostMapping
+	@PostMapping("/create")
 	public User createUser(@RequestBody User user) {
 		return userService.createUser(user);
 	}
