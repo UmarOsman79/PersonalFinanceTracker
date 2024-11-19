b82ec4693a4567042f35802e1d520e487e004685
Umar Osman <umarosman77@gmail.com>
2024-11-19 20:51:08 +0200

    Cleaning up code

--- src/main/java/com/gmail/umarosman77/pft/controller/UserController.java
+++ src/main/java/com/gmail/umarosman77/pft/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-	private UserService userService;
+	private final UserService userService;
 
 	@Autowired
 	public UserController(UserService userService) {
--- src/main/java/com/gmail/umarosman77/pft/exception/PftException.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.umarosman77.pft.exception;
-
-import lombok.Data;
-
-@Data
-public class PftException extends Exception {
-
-}
